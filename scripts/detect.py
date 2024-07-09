f2ffaadc08c887cd50381fdbc558a854116c6e6b
Moises <mcasaverde@unsa.edu.pe>
2024-07-09 14:01:55 -0500

    wip

--- scripts/detect.py
+++ scripts/detect.py
@@ -14,10 +14,11 @@ FromEmailId=""
 ToEmailId=""
 Password=""
 StreetName=""
+tf.compat.v1.disable_eager_execution()
 
 def load_graph(model_file):
   graph = tf.Graph()
-  graph_def = tf.GraphDef()
+  graph_def = tf.compat.v1.GraphDef()
 
   with open(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
@@ -30,7 +31,7 @@ def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
+  file_reader = tf.io.read_file(file_name, input_name)
   if file_name.endswith(".png"):
     image_reader = tf.image.decode_png(file_reader, channels = 3,
                                        name='png_reader')
@@ -44,24 +45,24 @@ def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
                                         name='jpeg_reader')
   float_caster = tf.cast(image_reader, tf.float32)
   dims_expander = tf.expand_dims(float_caster, 0);
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  resized = tf.compat.v1.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
+  sess = tf.compat.v1.Session()
   result = sess.run(normalized)
 
   return result
 
 def load_labels(label_file):
   label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  proto_as_ascii_lines = tf.io.gfile.GFile(label_file).readlines()
   for l in proto_as_ascii_lines:
     label.append(l.rstrip())
   return label
 
 if __name__ == "__main__":
-  file_name = "tf_files/flower_photos/daisy/3475870145_685a19116d.jpg"
-  model_file = "tf_files/retrained_graph.pb"
-  label_file = "tf_files/retrained_labels.txt"
+  file_name = "files/flower_photos/daisy/3475870145_685a19116d.jpg"
+  model_file = "files/retrained_graph.pb"
+  label_file = "files/retrained_labels.txt"
   input_height = 224
   input_width = 224
   input_mean = 128
@@ -112,7 +113,8 @@ if __name__ == "__main__":
   input_operation = graph.get_operation_by_name(input_name);
   output_operation = graph.get_operation_by_name(output_name);
 
-  with tf.Session(graph=graph) as sess:
+
+  with tf.compat.v1.Session(graph=graph) as sess:
     start = time.time()
     results = sess.run(output_operation.outputs[0],
                       {input_operation.outputs[0]: t})
